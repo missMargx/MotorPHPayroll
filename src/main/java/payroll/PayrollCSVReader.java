83b24edc952a78926b4d246d8b76a0064e0cb09c
Angel Lantin <lr.malantin@mmdc.mcl.edu.ph>
2026-05-22 14:42:57 +0800

    Test class for reader
    Adding test class to check if  PayrollCSVReader class is working.

--- src/main/java/payroll/PayrollCSVReader.java
+++ src/main/java/payroll/PayrollCSVReader.java
@@ -1,12 +1,59 @@
 
 package payroll;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 /**
  * Initial commit for reading csv file
  * Reader class
  */
 public class PayrollCSVReader {
     
-    private final String employeeFile =
-        "resources/MotorPH_Employee Data - Employee Details.csv";
+    private final String employeeFile = "resources/MotorPH_Employee Data - Employee Details.csv";
+    
+    public String[][] getEmployees() {
+
+        String[][] employees = new String[50][2];
+
+        try {
+
+            BufferedReader br = new BufferedReader(
+                    new FileReader(employeeFile));
+
+            String line;
+
+            br.readLine();
+
+            int row = 0;
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(",");
+
+                // Check if row has enough columns
+                if (data.length > 2) {
+
+                    employees[row][0] = data[1].trim(); // Last Name
+                    employees[row][1] = data[2].trim(); // First Name
+
+                    // Print to terminal please!!!!
+                    System.out.println(
+                            employees[row][0] + ", " +
+                            employees[row][1]);
+
+                    row++;
+                }
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Error reading CSV file.");
+            e.printStackTrace();
+        }
+
+        return employees;
+    }
 }
