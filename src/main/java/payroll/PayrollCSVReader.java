a4e4e8ff2ffe33f494d823756f6df6e432c92cbb
Angel Lantin <lr.malantin@mmdc.mcl.edu.ph>
2026-05-22 19:37:34 +0800

    Clean up
    - Removed duplicate file

--- src/main/java/payroll/PayrollCSVReader.java
+++
@@ -1,9 +0,0 @@
-
-package payroll;
-
-/**
- *Reader class for PayrollStaffDashboard
- */
-public class PayrollCSVReader {
-    private final String employeeFile = "resources/MotorPH_Employee Data - Employee Details.csv";
-}
