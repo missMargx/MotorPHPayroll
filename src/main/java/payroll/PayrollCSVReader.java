a3faf526748df7a8b586ad03d4162a16a19b2602
Angel Lantin <lr.malantin@mmdc.mcl.edu.ph>
2026-05-22 19:34:54 +0800

    Reapply "Reader class"
    This reverts commit 7e9150d42a22aadc69662843ec85ea2b8c50aebb.

---
+++ src/main/java/payroll/PayrollCSVReader.java
@@ -0,0 +1,9 @@
+
+package payroll;
+
+/**
+ *Reader class for PayrollStaffDashboard
+ */
+public class PayrollCSVReader {
+    private final String employeeFile = "resources/MotorPH_Employee Data - Employee Details.csv";
+}
