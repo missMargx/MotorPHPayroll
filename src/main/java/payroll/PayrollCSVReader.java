9a808c702d520bda45d214fa4563e441138622a7
Angel Lantin <lr.malantin@mmdc.mcl.edu.ph>
2026-05-22 15:30:56 +0800

    Implemented PayrollCSVReader to load employee data from CSV file
    - Created PayrollCSVReader class
    - Added functionality to read employee data from csvfile
    - Loaded Employee ID, Last Name, First Name, and Basic Salary
    - Fixed incorrect column indexing in employee array
    - Replaced String.split() with Apache Commons csvparser
    - Added csv header skipping
    - Improved csv parsing for quoted values and commas
    - Updated csv format syntax to an updated reader library

--- src/main/java/payroll/PayrollCSVReader.java
+++ src/main/java/payroll/PayrollCSVReader.java
@@ -1,51 +1,50 @@
 package payroll;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-/**
- * Initial commit for reading csv file
- * Reader class
- */
+import java.io.Reader;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
 public class PayrollCSVReader {
-    
-    private final String employeeFile = "resources/MotorPH_Employee Data - Employee Details.csv";
-    
+
+    private final String employeeFile =
+            "resources/MotorPH_Employee Data - Employee Details.csv";
+
     public String[][] getEmployees() {
 
         String[][] employees = new String[100][4];
 
         try {
 
-            BufferedReader br = new BufferedReader(
-                    new FileReader(employeeFile));
+            Reader reader = new FileReader(employeeFile);
 
-            String line;
-
-            br.readLine();
+         CSVParser csvParser = new CSVParser(
+            reader,
+            CSVFormat.DEFAULT.builder()
+                .setHeader()
+                .setSkipHeaderRecord(true)
+                .build()
+            );
 
             int row = 0;
 
-            while ((line = br.readLine()) != null) {
+            for (CSVRecord record : csvParser) {
 
-                String[] data = line.split(",");
+                employees[row][0] = record.get(0).trim();   // Employee ID
+                employees[row][1] = record.get(1).trim();   // Last Name
+                employees[row][2] = record.get(2).trim();   // First Name
+                employees[row][3] = record.get(13).trim();  // Basic Salary
 
-                // Check if row has enough columns
-                if (data.length > 2) {
-
-                    employees[row][0] = data[0]; // Employee ID
-                    employees[row][0] = data[1].trim(); // Last Name
-                    employees[row][1] = data[2].trim(); // First Name
-                    employees[row][3] = data[13]; // Basic Salary
-                   
-                    row++;
-                }
+                row++;
             }
 
-            br.close();
+            csvParser.close();
 
         } catch (IOException e) {
-            
+
             System.out.println("Error reading CSV file.");
             e.printStackTrace();
         }
