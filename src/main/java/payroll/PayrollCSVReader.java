474b81650df15a427d756b3dc71808a7387c4cb9
Angel Lantin <lr.malantin@mmdc.mcl.edu.ph>
2026-05-22 14:17:38 +0800

    CSV File Reader
    Initial commit new branch for csv reader

---
+++ src/main/java/payroll/PayrollCSVReader.java
@@ -0,0 +1,12 @@
+
+package payroll;
+
+/**
+ * Initial commit for reading csv file
+ * Reader class
+ */
+public class PayrollCSVReader {
+    
+    private final String employeeFile =
+        "resources/MotorPH_Employee Data - Employee Details.csv";
+}
